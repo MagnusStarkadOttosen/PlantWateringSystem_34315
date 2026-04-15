97ced9dc077663779c2c8e58dedbafc334a65c52
Mads Hauberg <mail@hotbear.org>
2026-04-15 15:53:03 +0200

    Made a Secrets.h
    For keeping API secrets etc.

---
+++ Secrets.example.h
@@ -0,0 +1,15 @@
+/*
+    Example file for generating Secrets.h file.
+*/
+
+/*
+    WIFI SSID + Password
+*/
+#define WIFI_SSID "mynetwork"
+#define WIFI_PASSWORD "mypassword"
+
+/*
+    Thingspeak API
+*/
+#define THINGSPEAK_API_KEY "mykey"
+#define THINGSPEAK_CHANNEL_ID mychannelid
