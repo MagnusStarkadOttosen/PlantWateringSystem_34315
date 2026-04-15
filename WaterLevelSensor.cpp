524d7d0e76431eb7167f47461d1098f9d4e7d71f
KillerAG99 <149151358+KillerAG99@users.noreply.github.com>
2026-04-15 19:24:53 +0530

    Refactor WaterLevelSensor code with power control
    Updated comments and added pin D1 for power control.

--- WaterLevelSensor.cpp
+++ WaterLevelSensor.cpp
@@ -1,14 +1,19 @@
-// // Sensor input taken on pin A0 on ESP12
+// // Sensor input taken on pin A0 on ESP12, Power provided by digital pin D1 so that the sensor is not on all the time
 
 // void setup() {
 //   // put your setup code here, to run once:
 //   Serial.begin(115200);
 //   pinMode(A0, INPUT);
+//   pinMode(D1, OUTPUT);
+//   digitalWrite(D1, LOW);
 // }
 
 // void loop() {
 //   // put your main code here, to run repeatedly:
+//   digitalWrite(D1, HIGH);
+//   delay(50);
 //   int sensorValue = analogRead(A0);
+//   digitalWrite(D1, LOW);
 
 //   // // Convert the sensor value to a percentage (0 to 100%)
 //   float waterLevelPercentage = (sensorValue / 4095.0) * 100;
@@ -18,5 +23,5 @@
 //   Serial.print(sensorValue);
 //   Serial.print("  Water Level Percentage: ");
 //   Serial.println(waterLevelPercentage);
-//   delay(500);
+//   delay(2000);
 // }
