4d07724377e9ebbb36bb37fd15bd5387cd6fb8d8
Mads Toft Hauberg <codeberg.folk800@passmail.net>
2026-04-22 20:36:40 +0200

    Made a simple webserver

---
+++ webserver/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/update', (req, res) => {
+  console.log(req.body);
+
+  res.status(200);
+});
+
+app.listen(8080, () => {
+  console.log('REST API server running on port 8080');
+}); 
